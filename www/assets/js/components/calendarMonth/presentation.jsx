50cb41497e612f086d92d198d47d83ac060adcc7
steveltomlin <steveltomlin@gmail.com>
2016-07-19 10:11:05 +0100

    tidy up styles for calendar so it can be used as either a month or week view

--- www/assets/js/components/calendarMonth/presentation.jsx
+++ www/assets/js/components/calendarMonth/presentation.jsx
@@ -16,21 +16,9 @@ export default class ComponentXPresentation extends Component {
       				<th>S</th>
       				<th>S</th>
       			</tr>
-      		</thead>
-      		<tfoot>
-      			<tr>
-      				<td data-unit-type="unitG">8</td>
-      				<td data-unit-type="unitG">9</td>
-      				<td data-unit-type="unitG">10</td>
-      				
-      				<td data-unit-type="unitY">11</td>
-      				<td data-unit-type="unitY">12</td>
-      				<td data-unit-type="unitY">13</td>
-      				<td data-unit-type="unitY">14</td>
-      			</tr>
-      		</tfoot>      		
+      		</thead>     		
       		<tbody>
-      			<tr className="topicUnits">
+      			<tr className="splitWeek">
       				<td data-unit-type="unitG" colSpan="3"> 
                                     <span className="wk">Wk 1 first half:</span>
                                     <span className="unitName">Algebra, quadratics, rearanging formulae, and identities</span>
@@ -40,17 +28,50 @@ export default class ComponentXPresentation extends Component {
                                     <span className="wk">Wk 1 second half:</span>
                                     <span className="unitName">Algebra, quadratics, rearanging formulae, and identities</span>
                               </td>                              
-      			</tr> 
-                        <tr className="assignments">
-                              <td data-unit-type="unitG"> <i className="spot"></i> </td>
-                              <td data-unit-type="unitG">  &nbsp;</td>
-                              <td data-unit-type="unitG"> &nbsp; </td>
+      			</tr>   
+                        <tr className="dates">
+                              <td data-unit-type="unitG">
+                                    <i className="spot"></i> 
+                                    <span className="date">8</span>
+                              </td>
+                              <td data-unit-type="unitG">9</td>
+                              <td data-unit-type="unitG">10</td>
+                              
+                              <td data-unit-type="unitY">11</td>
+                              <td data-unit-type="unitY">12</td>
+                              <td data-unit-type="unitY">13</td>
+                              <td data-unit-type="unitY">14</td>
+                        </tr>
 
-                              <td data-unit-type="unitY">  &nbsp;</td>
-                              <td data-unit-type="unitY"> <i className="spot"></i> </td>
-                              <td data-unit-type="unitY"> &nbsp; </td>
-                              <td data-unit-type="unitY"> &nbsp; </td>
-                        </tr>                             			
+                        <tr className="splitWeek">
+                              <td> 
+                                    &nbsp;                                 
+                              </td>
+                              <td> 
+                                   &nbsp;                                 
+                              </td>
+                              <td> 
+                                   &nbsp;                                 
+                              </td>                              
+                              <td data-unit-type="unitB" colSpan="4"> 
+                                    <span className="wk">Wk 1 second half:</span>
+                                    <span className="unitName">Algebra, quadratics, rearanging formulae, and identities</span>
+                              </td>                              
+                        </tr> 
+
+                        <tr className="dates">
+                              <td>15</td>
+                              <td>16</td>
+                              <td>17</td>
+                              
+                              <td data-unit-type="unitB">
+                                    <i className="spot"></i> 
+                                    <span className="date">18</span> 
+                              </td>
+                              <td data-unit-type="unitB">19</td>
+                              <td data-unit-type="unitB">20</td>
+                              <td data-unit-type="unitB">21</td>
+                        </tr>
       		</tbody>
       	</table>
 
