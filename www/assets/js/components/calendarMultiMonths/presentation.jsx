928d1e85245669240b2f5fe234f49f38dd0ce9b2
steveltomlin <steveltomlin@gmail.com>
2016-07-19 12:42:09 +0100

    final touches

--- www/assets/js/components/calendarMultiMonths/presentation.jsx
+++ www/assets/js/components/calendarMultiMonths/presentation.jsx
@@ -7,11 +7,50 @@ export default class ComponentXPresentation extends Component {
   render() {
     return (
       <div className="calendarMultiMonths">
+
+        <div className="headerOfDays">
+          <section className="calendarMonth">
+            <table>
+              <thead>
+                <tr>
+                  <th>M</th>
+                  <th>T</th>
+                  <th>W</th>
+                  <th>T</th>
+                  <th>F</th>
+                  <th>S</th>
+                  <th>S</th>
+                </tr>
+              </thead>
+            </table>
+          </section>
+          <section className="calendarMonth">
+            <table>
+              <thead>
+                <tr>
+                  <th>M</th>
+                  <th>T</th>
+                  <th>W</th>
+                  <th>T</th>
+                  <th>F</th>
+                  <th>S</th>
+                  <th>S</th>
+                </tr>
+              </thead>
+            </table>
+          </section>                
+        </div>
+
+        <div className="monthList">
+
            <CalendarMonth/>
            <CalendarMonth/>
            <CalendarMonth/>
            <CalendarMonth/>
-      </div>
+
+        </div>
+
+    </div>
     )
   }
 }
