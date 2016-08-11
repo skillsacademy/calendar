1e64b0a1f44e7a3740b9125585424492c58939eb
steveltomlin <steveltomlin@gmail.com>
2016-08-11 11:34:42 +0100

    adjusting calendar styling so that the days overlay the half weeks to be clickable

--- www/assets/js/components/calendar/container.jsx
+++ www/assets/js/components/calendar/container.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import CalendarMonth from 'js/components/calendarMonth/container.jsx'
 import CalendarAgenda from 'js/components/calendarAgenda/container.jsx'
 
 
@@ -7,6 +8,7 @@ export default class CalendarContainer extends Component {
   render() {
     return (
     	<div className="calendarWrapper">
+    		<CalendarMonth dispatch={this.props.dispatch}/>
 			<CalendarAgenda dispatch={this.props.dispatch}/>			
 		</div>		
     )
--- www/assets/js/components/calendarMonth/presentation.jsx
+++ www/assets/js/components/calendarMonth/presentation.jsx
@@ -1,10 +1,13 @@
 import React, {Component, PropTypes} from 'react';
 
 export default class ComponentXPresentation extends Component {
+      onClick(event){
+console.log('tdclick = ', event.nativeEvent.target.innerHTML);
+      }
   render() {
     return (
       <section className="calendarMonth">
-      	<table>
+      	<table onClick={this.onClick.bind(this)}>
       		<caption>January</caption>
       		<thead>
       			<tr>
@@ -14,31 +17,96 @@ export default class ComponentXPresentation extends Component {
       				<th>T</th>
       				<th>F</th>
       				<th>S</th>
-      				<th>S</th>
+      				<th>Sunday</th>
       			</tr>
       		</thead>     		
       		<tbody>
       			<tr className="splitWeek">
-      				<td data-unit-type="unitG" colSpan="3"> 
+      				<td colSpan="3" data-unit-type="unitG">                                     
                                     <span className="wk">Wk 1</span>
-                                    <span className="unitName">Algebra, quadratics, rearanging formulae, and identities</span>
+                                    <span className="unitName">Algebra, quadratics, rearanging formulae, and identities</span>                            
                               </td>
 
-                              <td  colSpan="4"> &nbsp;</td>                              
+                              <td  colSpan="4"> 
+                                    <span className="wk">Wk 2</span>
+                              </td>                              
       			</tr>   
                         <tr className="dates">
-                              <td data-unit-type="unitG">
-                                    <i className="spot"></i> 
+                              <td >
+                                    <span className="spots">
+                                          <i className="spot"></i> 
+                                          <i className="spot"></i>
+                                          <i className="spot"></i>
+                                    </span>
                                     <span className="date">8</span>
                               </td>
-                              <td data-unit-type="unitG">9</td>
-                              <td data-unit-type="unitG">10</td>
-                              
-                              <td>11</td>
-                              <td>12</td>
-                              <td>13</td>
-                              <td>14</td>
+                              <td><span className="date">9</span></td>
+                              <td><span className="date">10</span></td>
+                              <td><span className="date">11</span></td>
+                              <td><span className="date">12</span></td>
+                              <td><span className="date">13</span></td>
+                              <td><span className="date">14</span></td>
                         </tr>
+
+                        <tr className="splitWeek">
+                              <td colSpan="3" data-unit-type="unitB">                                    
+                                    <span className="wk">Wk 3</span>
+                                    <span className="unitName">Algebra, quadratics, rearanging formulae, and identities</span>                    
+                              </td>
+
+                              <td colSpan="4" data-unit-type="unitY">                                    
+                                    <span className="wk">Wk 4</span>
+                                    <span className="unitName">Quizes by the dozen where lots of exercises are great.</span>                    
+                              </td>                              
+                        </tr>   
+                        <tr className="dates">                              
+                              <td><span className="date">15</span></td>
+                              <td><span className="date">16</span></td>
+                              <td><span className="date">17</span></td>
+                              <td><span className="date">18</span></td>
+                              <td><span className="date">19</span></td>
+                              <td><span className="date">20</span></td>
+                        </tr>  
+
+                        {/* empty weeks example ....*/}
+                        <tr className="splitWeek">
+                              <td colSpan="3">                                    
+                                    <span className="wk">Wk 5</span>                 
+                              </td>
+
+                              <td colSpan="4">                                    
+                                    <span className="wk">Wk 6</span>                   
+                              </td>                               
+                        </tr>   
+                        <tr className="dates">
+                              <td ><span className="date">21</span></td>
+                              <td><span className="date">22</span></td>
+                              <td><span className="date">23</span></td>
+                              <td><span className="date">24</span></td>
+                              <td><span className="date">25</span></td>
+                              <td><span className="date">26</span></td>
+                              <td><span className="date">27</span></td>
+                        </tr>    
+
+                        {/* empty weeks example ....*/}
+                        <tr className="splitWeek">
+                              <td colSpan="3">                                    
+                                    <span className="wk">Wk 7</span>                 
+                              </td>
+
+                              <td colSpan="4">                                    
+                                    <span className="wk">Wk 8</span>                   
+                              </td>                               
+                        </tr>   
+                        <tr className="dates">
+                              <td ><span className="date">28</span></td>
+                              <td><span className="date">29</span></td>
+                              <td><span className="date">30</span></td>
+                              <td><span className="date">31</span></td>
+                              <td><span className="date">1</span></td>
+                              <td><span className="date">2</span></td>
+                              <td><span className="date">3</span></td>
+                        </tr>                                                                   
       		</tbody>
       	</table>
 
